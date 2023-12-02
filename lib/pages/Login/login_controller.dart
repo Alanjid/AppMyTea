a5fb14697736b933d5392a0bcb74e87ac76b6c76
SpiiderJoss <montalvanzavala@gmail.com>
2023-12-01 18:41:25 -0600

    Login funcional con backend

--- lib/pages/Login/login_controller.dart
+++ lib/pages/Login/login_controller.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:untitled/enviroment/response_api.dart';
+import 'package:untitled/providers/users_provider.dart';
 
 // ignore: camel_case_types
 class loginController extends GetxController {
   TextEditingController userController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  UsersProvider usersProvider = UsersProvider();
+
   void goToHelloPage() {
     Get.toNamed('/saludo');
-  }
-
-  void login() {
-    String user = userController.text.trim();
-    String password = passwordController.text.trim();
-
-    print('Email ${user}');
-    print('Password ${password}');
-
-    if (isValidForm(user, password)) {
-      goToHelloPage();
-    }
   }
 
   bool isValidForm(String user, String password) {
@@ -33,4 +25,26 @@ class loginController extends GetxController {
     }
     return true;
   }
+
+  void login() async {
+    try {
+      String username = userController.text.trim();
+      String password = passwordController.text.trim();
+      print('Username: $username');
+      print('Password: $password');
+
+      ResponseApi responseApi = await usersProvider.login(username, password);
+      print('Response Api: ${responseApi.toJson()}');
+
+      if (responseApi.success == true) {
+        goToHelloPage();
+      } else {
+        Get.snackbar('Inicio de sesión fallido', responseApi.message ?? '');
+      }
+    } catch (e) {
+      print('Error en la solicitud: $e');
+      Get.snackbar('Error', 'Hubo un problema al realizar la solicitud');
+    }
+  }
+
 }
--- lib/providers/users_provider.dart
+++ lib/providers/users_provider.dart
@@ -1,5 +1,34 @@
-import 'package:get/get.dart';
+import 'dart:io';
 
+import 'package:get/get.dart';
+import 'package:untitled/enviroment/response_api.dart';
+import '../enviroment/enviroment.dart';
+import '../moldes/user.dart';
+import 'package:http/http.dart' as http;
 class UsersProvider extends GetConnect {
-  
+  String get url => Enviroment.API_URL+"/paciente";
+
+  //MOTODO DE CREAR USUARIO MEDIANTE APP, NO SE USA PERO DEJO IMPLEMENTADO PARA FUTURO
+  Future<Response> create(User user) async {
+    Response response = await post('$url/create', user.toJson(),
+        headers: {'Content-Type': 'application/json'});
+    return response;
+  }
+
+  //METODO DE INICIO DE SESION/LOGIN
+  Future<ResponseApi> login(String username, String password) async {
+    Response response = await post(
+        '$url/login', {'username': username, 'password': password},
+        headers: {'content-length': 'application/json',
+        'HttpHeaders': 'authorizationHeader'});
+
+    if (response.body == null) {
+      print('Error: No se pudo ejecutar la peticion');
+      return ResponseApi();
+    }
+    ResponseApi responseApi = ResponseApi.fromJson(response.body);
+    print(responseApi.data);
+    return responseApi;
+
+}
 }
